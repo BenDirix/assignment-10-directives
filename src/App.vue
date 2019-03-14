a678dcc4c444f6bce713a5025ee76d4b50b902aa
Benjamin Dirix <benjamin.dirix@ad.core.vdabantwerpen.intra>
2019-03-14 15:13:32 +0100

    first and last commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <p v-customOn:click="isAFunction">click me</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    data(){
+    return{
+      notAFunction: 'Not a function'
+    }
+  },
+
+  methods: {
+    isAFunction() {
+      return "This text was changed by my custom v-on directive";
+    }
+  },
+  directives:{
+    "customOn": {
+      bind(el, binding, vnode){
+        if(typeof binding.value == "function"){
+          let event = binding.arg;
+          el.addEventListener(event, ()=>{
+            el.innerHTML = binding.value();
+          })
+        }else{
+          console.log('not a function');
+        }
+      }
+
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
